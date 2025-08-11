6e5f0b6ce967dd6688932acd32946dc7610dd37c
kousha-naji <koosha.naji@gmail.com>
2025-08-11 17:01:04 +0330

    added solution of first project

--- first-project/test.py
+++ first-project/test.py
@@ -1,4 +1,22 @@
-from kousha_queue import Queue
+def vowel_finder(word):
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+    vowels_in_word = [char for char in word.lower() if char in vowels]
+    if vowels_in_word == vowels:
+        words_found.append(word)
+    return ''
 
-my_queue = Queue()
-print(my_queue.is_empty())
+words_found = []
+
+
+try:
+    with open('ipsum.txt', 'r') as f:
+        content = f.read()
+except FileNotFoundError:
+    print("File not found!")
+
+words = content.split()
+uppercase_words = [word.upper() for word in words]
+for word in uppercase_words:
+    vowel_finder(word)    
+
+print(words_found)  
